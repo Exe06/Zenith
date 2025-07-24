2439f68072348b14d37852e9dc3cc2664b08f14f
Exe06 <exequielruiz0660@gmail.com>
2025-07-24 17:07:00 -0300

    creacion

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navList = document.querySelector('.navbar ul');
+
+  if (menuToggle && navList) {
+    menuToggle.addEventListener('click', () => {
+      navList.classList.toggle('active');
+    });
+  }
+});
