06fc944f8bf2594209f09bd9ec13324c353088b7
Exe06 <exequielruiz0660@gmail.com>
2025-08-10 12:28:02 -0300

    Creación de rutas user

--- src/app.js
+++ src/app.js
@@ -19,9 +19,11 @@
 import express from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import indexRoutes from './routes/index.js';
 import 'dotenv/config';
+import methodOverride from 'method-override';
 import { checkConnection } from './utils/checkConnection.js';
+import indexRoutes from './routes/index.js';
+import userRoutes from './routes/user.js';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -31,10 +33,12 @@ app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, '../public')));
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-app.use(indexRoutes);
+app.use("/", indexRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,12 +4,17 @@ const router = Router();
 
 // Registro
 router.get('/register', (req, res) => {
-  res.render('register');
+  res.render('register', {  title: 'Registro', stylesheet: '/css/register.css' });
 });
 
 // Inicio de Sesión
 router.get('/login', (req, res) => {
-  res.render('login');
+  res.render('login', { title: 'Iniciar Sesión', stylesheet: '/css/login.css' });
+});
+
+// Olvidé mi contraseña
+router.get('/forgot_pass', (req, res) => {
+  res.render('forgot_pass', { title: 'Olvidé mi contraseña', stylesheet: '/css/forgot_pass.css' });
 });
 
 export default router;
